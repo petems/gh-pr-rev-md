d8a13dc77f5920919b0e16503d658d269ed520ee
Peter Souter <petems@users.noreply.github.com>
2025-08-26 21:26:01 +0100

    style: apply ruff fixes and black formatting

--- docs/conf.py
+++ docs/conf.py
@@ -5,28 +5,28 @@ import sys
 from datetime import datetime
 
 # Add project root to sys.path if needed
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath(".."))
 
-project = 'gh-pr-rev-md'
-author = 'Peter Souter'
+project = "gh-pr-rev-md"
+author = "Peter Souter"
 current_year = datetime.now().year
-copyright = f'{current_year}, {author}'
+copyright = f"{current_year}, {author}"
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
 
 # Autodoc options
 autodoc_default_options = {
-    'members': True,
-    'undoc-members': True,
-    'show-inheritance': True,
+    "members": True,
+    "undoc-members": True,
+    "show-inheritance": True,
 }
